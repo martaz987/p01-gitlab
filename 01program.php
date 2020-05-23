5312be4b46d5bd6ba27d81f5425136f39e37c194
martaz987 <65817941+martaz987@users.noreply.github.com>
2020-05-23 16:46:49 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -3,6 +3,6 @@ echo ("Witaj");
 echo ("<br> Jestem programem 01");
 echo ("<br> TEST987");
 echo("<br> test987");
-echo("<br>Dalsza czesc programu")
+echo("<br>Dalsza czesc programu");
 echo("<br>Dalsza czesc progeramu 987 w GitHub");
 ?>
